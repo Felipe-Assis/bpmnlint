0314492311b66f2457c54d8596780dc120eea3df
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 04:55:03 -0300

    update the 2 recently added rules (no-manual-task & target-namespace)

--- rules/no-manual-task.js
+++ rules/no-manual-task.js
@@ -9,7 +9,6 @@ const {
 module.exports = function() {
 
   function check(node, reporter) {
-  	reporter.report(node.id, 'This will report an error every time!');
     if (is(node, 'bpmn:ManualTask')) {
       reporter.report(node.id, 'Element has disallowed type bpmn:ManualTask');
     }
