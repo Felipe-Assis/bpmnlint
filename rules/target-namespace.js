1af7055435d01733b4411074fb3831dbecda5cad
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 05:26:50 -0300

    Update target-namespace.js

--- rules/target-namespace.js
+++ rules/target-namespace.js
@@ -8,7 +8,7 @@ const {
  */
 module.exports = function() {
   function check(node, reporter) {
-  	reporter.report('aaaa', 'TEST');
+  	reporter.report(mode.id, 'TEST');
     if (is(node, 'bpmn:Definitions') && !node.targetNamespace) {
       reporter.report(node.id, 'Element is missing targetNamespace');
     }
