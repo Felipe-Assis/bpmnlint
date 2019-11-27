317fc2dce7c2b982db758fb6f548f9944c3d0779
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 05:42:23 -0300

    Update unique-name.js

--- rules/unique-name.js
+++ rules/unique-name.js
@@ -2,14 +2,13 @@ const {
   is
 } = require('bpmnlint-utils');
 
-
+var nodeNames = [];
 //
 //	B.10 Two Activities in the same Process should not have the same name
 //
 
 module.exports = function() {
   function check(node, reporter) {
-  	var nodeNames;
   	console.log(nodeNames)
     if (nodeNames.includes(node.name) == true) {
       reporter.report(node.id, 'Model has duplicate activity name');
