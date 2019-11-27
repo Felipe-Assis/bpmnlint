5fc07f0d15faa306467136957fc460a6d6e9090b
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 05:23:46 -0300

    b

--- rules/unique-name.js
+++ rules/unique-name.js
@@ -3,11 +3,12 @@ const {
 } = require('bpmnlint-utils');
 
 
-/**
- * Rule that reports missing targetNamespace on bpmn:Definitions.
- */
+//
+//	B.10 Two Activities in the same Process should not have the same name
+//
+
 module.exports = function() {
-  console.log("TEST");
+	
   function check(node, reporter) {
     if (is(node, 'bpmn:Definitions') && !node.targetNamespace) {
       reporter.report(node.id, 'Element is missing targetNamespace');
