47eee201507623af437800aef19f08c4a53bb1be
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 05:24:50 -0300

    Update target-namespace.js

--- rules/target-namespace.js
+++ rules/target-namespace.js
@@ -7,7 +7,7 @@ const {
  * Rule that reports missing targetNamespace on bpmn:Definitions.
  */
 module.exports = function() {
-  console.log("TEST");
+  eporter.report('aaaa', 'TEST');
   function check(node, reporter) {
     if (is(node, 'bpmn:Definitions') && !node.targetNamespace) {
       reporter.report(node.id, 'Element is missing targetNamespace');
