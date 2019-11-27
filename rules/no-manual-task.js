12bd890edee170eb23202fd187dd7f7bb8e31451
Felipe-Assis <felipe9525@hotmail.com>
2019-11-27 04:21:20 -0300

    a

--- rules/no-manual-task.js
+++ rules/no-manual-task.js
@@ -9,6 +9,7 @@ const {
 module.exports = function() {
 
   function check(node, reporter) {
+  	reporter.report(node.id, 'This will report an error every time!');
     if (is(node, 'bpmn:ManualTask')) {
       reporter.report(node.id, 'Element has disallowed type bpmn:ManualTask');
     }
